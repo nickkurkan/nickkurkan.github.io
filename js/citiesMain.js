b5fcbd437b647c85bb3ae3afdc2c0e4bb811308a
Nikita <kurkan.n.s@gmail.com>
2018-09-08 17:45:30 +0300

    Adaptability, burger, footer

--- js/citiesMain.js
+++ js/citiesMain.js
@@ -2,15 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let ctsBtn = document.getElementsByClassName('btn-cities')[0],
      ctsList = document.getElementsByClassName('cities-other-list')[0],
-     menuBtn = document.getElementsByClassName('btn-menu');
+     menuBtn = document.getElementsByClassName('btn-menu')[0],
+     menuMobile = document.getElementsByClassName('menu-mobile')[0];
 
   let clicks = true;
-
+  let menuClicks = true;
   ctsBtn.addEventListener('click', function () {
     showCities();
   });
 
   menuBtn.addEventListener('click', function (e) {
+    e.preventDefault;
+    this.classList.toggle('btn-menu-active')
     showMenu();
   });
 
@@ -29,7 +32,13 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function showMenu () {
-    
+    if(menuClicks) {
+      menuMobile.classList.toggle('menu-mobile-active');
+      menuClicks = false;
+    } else {
+      menuMobile.classList.toggle('menu-mobile-active');
+      menuClicks = true;
+    }
   }
 
 });
