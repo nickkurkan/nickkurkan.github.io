851855e32b9d650fba9f05d58e0951dc4d495427
Nikita <kurkan.n.s@gmail.com>
2018-09-08 11:36:59 +0300

    Fix update

--- js/citiesMain.js
+++ js/citiesMain.js
@@ -1,12 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   let ctsBtn = document.getElementsByClassName('btn-cities')[0],
-    ctsList = document.getElementsByClassName('cities-other-list')[0];
+     ctsList = document.getElementsByClassName('cities-other-list')[0],
+     menuBtn = document.getElementsByClassName('btn-menu');
 
   let clicks = true;
 
   ctsBtn.addEventListener('click', function () {
     showCities();
+  });
+
+  menuBtn.addEventListener('click', function (e) {
+    showMenu();
   });
 
   function showCities() {
@@ -21,6 +26,10 @@ document.addEventListener('DOMContentLoaded', function () {
       ctsList.classList.remove('cities-other-list-active');
       ctsList.classList.add('cities-other-list');
     }
+  }
+
+  function showMenu () {
+    
   }
 
 });
